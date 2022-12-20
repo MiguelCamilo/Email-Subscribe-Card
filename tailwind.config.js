0f8fba7f24e441162374627bdb59ad07515e24d1
Miguel C <52728963+MiguelCamilo@users.noreply.github.com>
2022-12-19 23:14:21 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./build/**/*.{js,jsx,ts,tsx,pug,html}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
